9b052392029fb048a71b0d6fbce3241b7ab65101
Leraedon <thor0249@gmail.com>
2024-05-04 22:11:24 +0900

    lecture 96 to 103

---
+++ section5/section5_project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/investment-calculator-logo.png';
+
+export default function Header() {
+  return <header>
+    <img src={logo} alt="Logo showing a money bang" />
+    <h1>React Investment Calculator</h1>
+  </header>
+}
